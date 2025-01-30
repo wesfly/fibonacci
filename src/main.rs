92721558838baf31298468c5a620822683b2d5e1
wesfly <wesleydeanford@yahoo.com>
2025-01-30 18:44:58 +0100

    removed unneccesary lines

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,3 @@
-// i rewrote it from a cpp file
 fn fib(n: i128) -> i128 {
     let mut a: i128 = 0;
     let mut b: i128 = 1;
@@ -16,14 +15,10 @@ fn fib(n: i128) -> i128 {
 }
 
 fn main() {
-    //let n = 9;
-
-    //let x = fib(n);
     for i in 0..186 {
-        // NEVER let it run over 186 iterations, it'll panic
+        // NEVER let it run over 186 iterations, or it'll panic
         let x: i128 = fib(i);
         println!("Iteration: {}", i + 1);
         println!("{:#?}", x);
     }
-    //println!("{:#?}", x);
 }
